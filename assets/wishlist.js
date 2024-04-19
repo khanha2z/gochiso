7df9e942ac7e6e0ad54eb33b493424147b290fab
A2ZTech <khanhlq@a2ztech.vn>
2024-04-20 04:39:54 +0700

    fix Wishlist

--- assets/wishlist.js
+++ assets/wishlist.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
 		wishlistProducts = [],
 		wishlistButton = document.querySelector('.view-wishlist-bottom-btn');
 
+	var shopUrl = window.location.hostname;
+
 	const hsbToHex = (hsb) => {
 		const { hue, saturation, brightness } = hsb; const h = hue / 360; const s = saturation; const v = brightness; const i = Math.floor(h * 6); const f = h * 6 - i; const p = v * (1 - s); const q = v * (1 - f * s); const t = v * (1 - (1 - f) * s); let r, g, b;
 		switch (i % 6) { case 0: r = v; g = t; b = p; break; case 1: r = q; g = v; b = p; break; case 2: r = p; g = v; b = t; break; case 3: r = p; g = q; b = v; break; case 4: r = t; g = p; b = v; break; case 5: r = v; g = p; b = q; break; }
