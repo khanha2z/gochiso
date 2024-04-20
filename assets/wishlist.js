4ba6b308077aaf1f371942bce84bba51d084183d
DESKTOP-8ORJVH2\Tien Pham <longdv@a2ztech.vn>
2024-04-20 08:51:23 +0700

    update

--- assets/wishlist.js
+++ assets/wishlist.js
@@ -708,7 +708,7 @@ document.addEventListener("DOMContentLoaded", () => {
 									<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
 								</svg>
 							</button>
-							<h2 class="popup-heading">ウィッシュリスト製品</h2>
+							<h2 class="popup-heading">お気に入り商品</h2>
 						</div>
 						<div class="popup-body">
 							<div class="popup-content">
